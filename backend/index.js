782c6f05b10567ed00410e0defa5e59832888839
viralhub420 <hshamimbd355@gmail.com>
2026-02-06 10:15:47 +0600

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,23 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
 
-import userRoutes from './user.js'
-import adsRoutes from './ads.js'
-import withdrawRoutes from './withdraw.js'
+import userRoutes from "./user.js";
+import adsRoutes from "./ads.js";
+import withdrawRoutes from "./withdraw.js";
 
-dotenv.config();
 const app = express();
-
 app.use(cors());
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Admingo backend running 🚀");
+});
 
 app.use("/user", userRoutes);
 app.use("/ads", adsRoutes);
 app.use("/withdraw", withdrawRoutes);
 
-app.get("/", (req, res) => {
-  res.send("🔥 Admingo Backend Running");
-});
-
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log("Server running on port", PORT);
 });
