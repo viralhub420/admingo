e347dd93c633cc64c48de9f2ea3f876f0288a70c
viralhub420 <hshamimbd355@gmail.com>
2026-02-06 12:10:54 +0600

    Update firebase.js

--- backend/firebase.js
+++ backend/firebase.js
@@ -1,9 +1,6 @@
 import admin from "firebase-admin";
-import fs from "fs";
 
-const serviceAccount = JSON.parse(
-  fs.readFileSync("./serviceAccountKey.json", "utf8")
-);
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
