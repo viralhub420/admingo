adb6ff9410ea8c38146633be4ab18eb14cc40063
viralhub420 <hshamimbd355@gmail.com>
2026-02-08 17:57:52 +0600

    Update firebase.js

--- backend/firebase.js
+++ backend/firebase.js
@@ -1,9 +1,15 @@
-import admin from "firebase-admin";
+// firebase.js
+const admin = require("firebase-admin");
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
-
+// Initialize Firebase Admin
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
+  })
 });
 
-export const db = admin.firestore();
+const db = admin.firestore();
+
+module.exports = { admin, db };
